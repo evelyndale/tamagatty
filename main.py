c049e85a4907e6c2213848841245af265a67969b
Evelyn <evelynvioletdale@gmail.com>
2020-06-20 17:22:51 -0700

    clean up notes

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import datetime
 pet_name = input("What is the name of your pet?")
 pet_color = input("What color is your pet?")
 
-"""Virtual 'dice' to generate random events"""
+# Virtual 'dice' to generate random events
 def random_event():
     dice_state = random.randrange(10)
     if(dice_state < 5):
@@ -15,23 +15,23 @@ def random_event():
         print("Your dice are seriously busted, friend")
     return(dice_state)
 
-"""Our pet constructor"""
+# Our pet constructor
 class MyPet:
     def __init__(self, name, color, species):
         self.name = name
         self.color = color
         self.species = species
 
-"""Create the user's pet using the pet constructor"""
+# Create the user's pet using the pet constructor
 pet_1 = MyPet(pet_name, pet_color, "cat")
 
-"""A function to list the basic properties of the user's pet"""
+# A function to list the basic properties of the user's pet
 def basic_pet_info():
     print(f"the name of your pet is {pet_1.name}")
     print(f"{pet_1.name} is a lovely {pet_1.color} {pet_1.species}!")
     print(f"{pet_1.name} was born on {datetime.datetime.now()}")
 
-"""Display the user's pet info after they make their pet"""
+# Display the user's pet info after they make their pet
 basic_pet_info()
 
 random_event()
